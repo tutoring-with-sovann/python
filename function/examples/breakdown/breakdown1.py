68d3b666bf94e8cc057649b8f93e21582cb7e07f
sovann72-dev <sovann72.dev@gmail.com>
2025-07-19 18:11:53 +0700

    add more

--- function/examples/breakdown/breakdown1.py
+++ function/examples/breakdown/breakdown1.py
@@ -1,4 +1,4 @@
-def printExample() -> None:
+def printExample(value: string) -> None:
   # Call underlying system to print something to the output.
   # Question: Can you assign a variable to a return value of print?
   # Yes, if both of them are None.
@@ -7,7 +7,7 @@ def printExample() -> None:
 # Let's look at this 'p' variable. 
 # When I wrote 'p: None', that means 'p' has the data type of 'None' which is nothing.
 # It doesn't hold any actual value.
-p: None = printExample();
+p: None = printExample('This is a string');
 
 
 nativePrint: None = print("Halo")
