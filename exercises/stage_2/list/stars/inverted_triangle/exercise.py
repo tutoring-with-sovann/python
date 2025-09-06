367fbd3ada41490f5c09e5623016e30c0721bc86
sovann72-dev <sovann72.dev@gmail.com>
2025-09-06 08:30:36 +0700

    tweak

--- exercises/stage_2/list/stars/inverted_triangle/exercise.py
+++ exercises/stage_2/list/stars/inverted_triangle/exercise.py
@@ -3,7 +3,7 @@
 # Write a Python function called inverted_triangle that takes one argument, level.
 # This function should print a inverted triangle based on the provided level argument.
 
-# Example output for inverted_pyramid(5)
+# Example output for inverted_triangle(5)
 # *****
 # ****
 # ***
